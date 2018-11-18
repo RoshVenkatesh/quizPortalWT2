9f4ca8414713029dbe18a0ecb85f0c6c603cc304
Roshni Venkatesh <vroshni@hotmail.com>
2018-11-18 18:51:41 +0530

    Quiz page

--- frontend/src/pages/quiz/quiz.ts
+++ frontend/src/pages/quiz/quiz.ts
@@ -17,10 +17,15 @@ import { Http, Headers } from '@angular/http';
 export class QuizPage {
 
   dummy_list: any;
-  items1: Array<{question:string,answer:string,title:string,options:Array<"">,fib:Number}>;
+  items1: Array<{question:string,answer:string,title:string,options:Array<"">,fib:Number, response: String}>;
+  title1 : String;
+  title2 : String;
+  title3 : String;
+  title4 : String;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController,) {
     this.items1 = [];
+    
   }
 
   ionViewDidLoad() {
@@ -42,21 +47,50 @@ export class QuizPage {
           .subscribe(res => {
            let data = res.json()['list'];
            for(let i in data){
+            let optionsArray = data[i].options
+            optionsArray.push(data[i].answer)
+            console.log(optionsArray)
+            shuffleArray(optionsArray)
                this.items1.push({
                  question : data[i].question,
                  answer : data[i].answer,
                  title : data[i].title,
                  fib : data[i].fib,
-                 options : data[i].options               
+                 options : optionsArray,
+                 response : ""             
              })                        
            }
            console.log(this.items1[0])
            loading.dismiss();
+           this.title1 = data[0].title;
+           this.title2 = data[5].title;
+           this.title3 = data[10].title;
+           this.title4 = data[15].title;
            // let ques=data['questions'];
             //traverse the questions array
           
 });
 
-  }
+}
 
+public checkAnswer(item, given_answer){
+  console.log(given_answer)
+
+  if (given_answer.toLowerCase() === item.answer){
+    console.log("Correct")
+  }
+  else{
+    console.log("Wrong")
+  }
+}
+
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
 }
