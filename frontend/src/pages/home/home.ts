5ead3bc423dda16ea2a81e2a5749fa6ca2977ced
Roshni Venkatesh <vroshni@hotmail.com>
2018-11-19 23:43:36 +0530

    Added Scoring and Graphs

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HelloIonicPage } from '../hello-ionic/hello-ionic';
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the HomePage page.
  *
@@ -15,13 +16,21 @@ import { HelloIonicPage } from '../hello-ionic/hello-ionic';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public storage:Storage) {
   }
 navigate(){
   this.navCtrl.push(HelloIonicPage,{loggedin:0})
 }
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+    this.storage.set('Cartoons', [0,0]);
+    this.storage.set('Chemistry', [0,0]);
+    this.storage.set('Comic', [0,0]);
+    this.storage.set('Human Body', [0,0]);
+    this.storage.set('Indian Cities', [0,0]);
+    this.storage.set('Monuments', [0,0]);
+    this.storage.set('Physics', [0,0]);
+    this.storage.set('Solar System', [0,0]);
   }
 
 }
--- frontend/src/pages/quiz/quiz.ts
+++ frontend/src/pages/quiz/quiz.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { Http, Headers } from '@angular/http';
 import { HelloIonicPage } from '../hello-ionic/hello-ionic';
+import { Storage } from '@ionic/storage';
+import { GraphsPage } from '../graphs/graphs';
 
 /**
  * Generated class for the QuizPage page.
@@ -18,15 +20,23 @@ import { HelloIonicPage } from '../hello-ionic/hello-ionic';
 export class QuizPage {
 
   dummy_list: any;
+  items2: Array<{name:string,topics:Array<"">,score:number,possible:number}>;
   items1: Array<{question:string,answer:string,title:string,options:Array<"">,fib:Number, response: String, correct:String}>;
   title1 : String;
   title2 : String;
   title3 : String;
   title4 : String;
   subjects : any;
+  topic: any;
+  name : any;
+  topics: any;
+  score : any;
+  possible : any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController,) {
+
+  constructor(public navCtrl: NavController, public storage:Storage,public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController) {
     this.items1 = [];
+    this.items2 = [];
     this.subjects = navParams.get('item');
     
   }
@@ -38,7 +48,6 @@ export class QuizPage {
       content: 'Please wait...'
     });
     loading.present();
-    this.dummy_list = ['Molecules', 'Hydrogen', 'Noble Gases',"Chemical Reaction", 'Atoms']
     let postParams = {'topics': this.subjects.topics}
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
@@ -81,20 +90,119 @@ export class QuizPage {
 public checkAnswer(item, given_answer){
 
   if (given_answer.toLowerCase() === item.answer.toLowerCase()){
-    console.log("Correct")
-    item.correct = 1
+    item.correct = "1"
   }
   else{
-    console.log("Wrong")
-    item.correct = 0
+    item.correct = "0"
   }
 
 }
 
 public continue(){
+
+  let x = 0;
+  for (let i = 0; i<20 ; i++){
+    if (this.items1[i].correct == "1"){
+      console.log("CORRECT")
+      x++;
+    }
+  }
+
+  console.log(x)
+  var current = [0,0]
+  console.log(this.subjects.name)
+  this.storage.get(this.subjects.name).then((value) => {
+
+    /*console.log("OLD")
+    console.log(value[0])
+    console.log(value[1])*/
+
+    current[0] = value[0] + x;
+    current[1] = value[1] + 20;
+
+    /*console.log("NEW")
+    console.log(current[0])
+    console.log(current[1])*/
+    
+
+  this.storage.set(this.subjects.name,[current[0], current[1]])
+
+});
   this.navCtrl.setRoot(HelloIonicPage,{loggedin:1})
 }
 
+public graph(){
+
+  let x = 0;
+  for (let i = 0; i<20 ; i++){
+    if (this.items1[i].correct == "1"){
+      console.log("CORRECT")
+      x++;
+    }
+  }
+
+  console.log(x)
+  var current = [0,0]
+  console.log(this.subjects.name)
+  this.storage.get(this.subjects.name).then((value) => {
+
+    current[0] = value[0] + x;
+    current[1] = value[1] + 20;    
+
+  this.storage.set(this.subjects.name,[current[0], current[1]])
+
+});
+
+let headers = new Headers();
+headers.append('Content-Type', 'application/json');
+
+    let url = "http://localhost:5000"
+    let path = url.concat( "/get_topics");
+    //console.log(postParams);
+    this.name = [];
+    this.topics = [];
+    this.score = [];
+    this.possible = [];
+
+    this.http.get(path, {headers: headers})
+      .subscribe(res => {
+       let data = res.json()['topics'];
+       //console.log(data)
+       for(let i in data){
+         console.log(i)
+         if(i!='_id'){
+          this.items2.push({
+            name : i,
+            topics:data[i],
+            score : 0,
+            possible : 0         
+        })   
+          }  
+        }     
+        
+        for (let i = 0; i< this.items2.length; i++ ){
+
+              this.storage.get(this.items2[i].name).then((value) => {  
+              console.log(this.items2[i].name,value[0],value[1])
+              this.setValue(i,value[0],value[1]);
+          });
+
+        }
+        
+       });
+      }
+       
+  public setValue(i,val1,val2){
+    console.log("HI BRO")
+    this.items2[i].score = val1;
+    this.items2[i].possible = val2;
+
+    if (i == 7){
+      console.log(this.items2)
+      this.navCtrl.setRoot(GraphsPage, {scores:this.items2})
+
+    }
+  }
 }
 
 function shuffleArray(array) {
